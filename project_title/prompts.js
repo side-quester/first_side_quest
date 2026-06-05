f92a8d734e14cc241f79f148bcc9b4ad8735e660
side_quester <289309688+side-quester@users.noreply.github.com>
2026-06-05 01:24:14 +0000

    add example prompts

---
+++ project_title/prompts.js
@@ -0,0 +1,8 @@
+const prompts = [
+  "perspective, 8k, girl, anime, keqing genshin impact, long purple hair, kawaii and sexy, realistic",
+  "8k, girl, anime, vivid color, perspective, toki, bunny girl, peace sign, beach, blue archive",
+  "perspective, 8k, girl, asuna, blue archive, anime, blue bow, sheer white long camisole, wind hitting her hair, long hair, kawaii and sexy, beach, golden hour",
+  "perspective, 8k, mature girl, anime, straw hat, white long camisole, wind hitting her hair, long hair, kawaii and sexy, beach",
+  "8k, perspective, striking violet-eyed sorceress, raven-black hair cascading in loose waves, sharp cheekbones, full lips painted dark plum, wearing a form-fitting black dress with intricate silver embroidery, signature obsidian star necklace, standing with regal poise amidst swirling magical energy",
+  "8k, perspective, mysterious man in his late 20s, sharp angular features, piercing silver eyes, tousled dark blue hair with silver streaks, wearing a tailored charcoal trench coat with glowing runic embroidery, standing atop a neon-lit rooftop in cyberpunk cityscape"
+];
